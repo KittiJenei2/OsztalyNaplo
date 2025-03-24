78c89630594868a70f54150dc8cb6eaba7916c1e
Bencevrg <130698341+Bencevrg@users.noreply.github.com>
2025-03-24 09:34:49 +0100

    Log in kezdetek

--- database/migrations/2025_03_18_105658_create_schoolclasses_table.php
+++ database/migrations/2025_03_18_105658_create_schoolclasses_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('schoolclasses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('year');
         });
     }
 
